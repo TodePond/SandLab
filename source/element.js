0e969e571e7839af8e0ce70051eb46517179285f
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-01-14 13:16:56 +0000

    down fallin

--- source/element.js
+++ source/element.js
@@ -1,11 +1,11 @@
 const ELEMENTS = new Map()
 
-const FALL_SPEED = 1 / 128
+const FALL_SPEED = 1 / 256
 
 const pointer = getPointer()
 const POINTER_RADIUS = 0.0 //0.03
 const POINTER_FADE_RADIUS = 0.0 //0.1
-const POINTER_CELL_SIZE = 1 / 32 //1 / 256
+const POINTER_CELL_SIZE = 1 / 128 //1 / 256
 let AIR_TARGET = 1 / 32
 const getPointerAirTarget = (cell) => {
 	if (pointer.position.x === undefined) {
@@ -107,7 +107,7 @@ ELEMENTS.set(GREY.splash, {
 ELEMENTS.set(YELLOW.splash, {
 	name: "Sand",
 	update: (cell, world) => {
-		const below = pickSnips(cell, world, "top", -FALL_SPEED)
+		const below = pickSnips(cell, world, "bottom", FALL_SPEED)
 
 		if (below.snips.length === 0) {
 			return tryToSleep(cell, world)
@@ -121,7 +121,7 @@ ELEMENTS.set(YELLOW.splash, {
 				//sand = merge([cell, ...above.snips])
 			}
 
-			const movedCells = swapSnips(sand, below.snips, "top")
+			const movedCells = swapSnips(sand, below.snips, "bottom")
 			//return world.replace([cell, ...below.contacts], [cell, ...below.excesses, ...below.snips])
 			return world.replace([cell, ...below.contacts], [...below.excesses, ...movedCells])
 		}
