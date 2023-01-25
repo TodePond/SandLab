dc1949c993f50a68485c469ed8d75365e04b4208
Luke Wilson <l2wilson94@gmail.com>
2023-01-25 14:41:50 +0000

    plant

--- source/element.js
+++ source/element.js
@@ -166,6 +166,23 @@ ELEMENTS.set(RED.splash, {
 ELEMENTS.set(GREEN.splash, {
 	name: "Plant",
 	key: ["p", "7"],
+	update: (cell, world) => {
+		const allContacts = [
+			...pickContacts(cell, world, "top"),
+			...pickContacts(cell, world, "bottom"),
+			...pickContacts(cell, world, "left"),
+			...pickContacts(cell, world, "right"),
+		]
+
+		const changed = []
+		for (const contact of allContacts) {
+			if (contact.colour.splash === BLUE.splash) {
+				const recoloured = recolour(contact, GREEN)
+				changed.push(...world.replace([contact], [recoloured]))
+			}
+		}
+		return changed
+	},
 })
 
 ELEMENTS.set(BLUE.splash, {
